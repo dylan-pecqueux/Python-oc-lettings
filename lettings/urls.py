cfe5e7740bd6fab03c8290f285f4413d9e3a9cf6
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-12-08 10:53:06 +0100

    transfer templates in new app and rename

--- lettings/urls.py
+++ lettings/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import lettings_index, letting
 
 urlpatterns = [
-    path("lettings/", lettings_index, name="lettings_index"),
+    path("lettings/", lettings_index, name="index"),
     path("lettings/<int:letting_id>/", letting, name="letting"),
 ]
--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import profiles_index, profile
 
 urlpatterns = [
-    path("profiles/", profiles_index, name="profiles_index"),
+    path("profiles/", profiles_index, name="index"),
     path("profiles/<str:username>/", profile, name="profile"),
 ]
