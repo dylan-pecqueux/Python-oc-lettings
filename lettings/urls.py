7fd6fe5d5ce464a0ac0e35188aaf561f8248c6db
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-12-08 11:39:59 +0100

    transfer templates and correct bugs

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import lettings_index, letting
+from .views import index, letting
 
 urlpatterns = [
-    path("lettings/", lettings_index, name="index"),
+    path("lettings/", index, name="lettings_index"),
     path("lettings/<int:letting_id>/", letting, name="letting"),
 ]
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import profiles_index, profile
+from .views import index, profile
 
 urlpatterns = [
-    path("profiles/", profiles_index, name="index"),
+    path("profiles/", index, name="profiles_index"),
     path("profiles/<str:username>/", profile, name="profile"),
 ]
