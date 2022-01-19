6877347483371a18dc90f655ae6fe536f96d09fc
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-01-19 11:04:27 +0100

    test pipeline

--- lettings/views.py
+++ lettings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Letting
 
+
 # Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit.
 # Sed non placerat massa.
 # Integer est nunc, pulvinar a tempor et, bibendum id arcu.
