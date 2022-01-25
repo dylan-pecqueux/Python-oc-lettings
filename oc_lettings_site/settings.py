33302b846ecef2b6d603be243a29f5dda40a1c97
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-01-25 17:00:47 +0100

    correct bug from staticfiles in prod

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -113,10 +113,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 sentry_sdk.init(
