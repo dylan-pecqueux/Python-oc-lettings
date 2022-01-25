cccc6c2167529f0ca961f41e7978f84e3c4a9e93
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-01-25 17:39:20 +0100

    correct bug from staticfiles in prod

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -6,7 +6,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
